3fae3a22492c74b9902dedc28a08c89cd5cc2379
Maykon Santiago <maykonsantiago.ms@gmail.com>
2022-02-24 23:01:26 -0300

    Dando funcionalidade ao feed editor

--- src/routes.php
+++ src/routes.php
@@ -10,3 +10,5 @@ $router->post('/login', 'LoginController@signinAction');
 
 $router->get('/cadastro', 'LoginController@signup');
 $router->post('/cadastro', 'LoginController@signupAction');
+
+$router->post('/post/new', 'PostController@new');
